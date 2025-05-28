c1919bf0e1856c66f832dc212c73be49b5916155
mirricoder <mirrifavor@gmail.com>
2025-05-28 21:44:33 +0300

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#0D1321',
+        secondary: '#F4A261',
+        accent: '#E76F51',
+        'bg-light': '#F9FAFB',
+        'text-main': '#333333',
+      },
+    },
+  },
+  plugins: [],
+}
